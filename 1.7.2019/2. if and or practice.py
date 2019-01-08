d25398298a54a5594e6e73ca1ecf80b1253804cc
hechtermach <36723756+hechtermach@users.noreply.github.com>
2019-01-07 21:33:07 -0500

    Add files via upload

---
+++ 1.7.2019/2. if and or practice.py
@@ -0,0 +1,14 @@
+user_name="HeMa"
+email="mach0425@outlook.com"
+password="12345"
+if ((user_name=="HeMa" or email=="mach0425@outlook.com") and password=="123456"):
+    print ("success")
+else:
+    print ("wrong name/password")
+if user_name=="HeMa":
+    print ("he is HeMa")
+user_name="Hema"
+if not user_name=="HeMa":
+    print ("he is not HeMa")
+if user_name!="HeMa":
+    print ("He is not HeMa")
---
+++ 1.7.2019/6. range for.py
@@ -0,0 +1,7 @@
+s=0
+for i in range (1,101):
+    s=s+i
+    if i==100:
+        print (s)
+
+    
